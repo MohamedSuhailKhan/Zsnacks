ffafe0b8724d4c7616f08726b7366ec20e3f6dba
Mohamed Suhail Khan <141369674+MohamedSuhailKhan@users.noreply.github.com>
2025-01-16 22:00:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const productLinks = document.querySelectorAll('.product-list .product');
 
 productLinks.forEach(product => {
-  product.addEventListener('click', function(event) {
-    event.preventDefault();
+    product.addEventListener('click', function (event) {
+        event.preventDefault();
 
-    this.classList.add('swipe-left'); // Add swipe-left class
+        this.classList.add('swipe-left');
 
-    const link = this.querySelector('a');
-    const href = link ? link.getAttribute('href') : null;
+        const link = this.querySelector('a');
+        const href = link ? link.getAttribute('href') : null;
 
-    setTimeout(() => {
-        if (href) {
-            window.location.href = href;
-        } else {
-            console.error("No link found in product block");
-        }
-    }, 500); // Match transition duration
-  });
+        setTimeout(() => {
+            if (href) {
+                window.location.href = href;
+            } else {
+                console.error("No link found in product block");
+            }
+        }, 1000);
+    });
 });
